c290d18e0dbf864b96f04184c41851c7a8ef53c8
Klimpavlov <Klimpavlov2002@gmail.com>
2025-02-13 10:46:10 +0300

    fix package.json file

--- backend/index.js
+++ backend/index.js
@@ -47,5 +47,5 @@ const initializeDefaultAdminUser = async() => {
 initializeDefaultAdminUser();
 
 sequelize.sync().then(() => console.log("Connected to db"));
-app.listen(3001, () => console.log("Server is running on port 3001"));
+app.listen(PORT, () => console.log("Server is running on port 3001"));
 
