cb369563d7bf83b06344f7fccb54bbd8bad2e3b0
Klimpavlov <Klimpavlov2002@gmail.com>
2025-02-17 00:31:29 +0300

    fix: personal page in progress

---
+++ frontend/src/app/api/users/getUserInfo.js
@@ -0,0 +1,17 @@
+import apiClient from "@/app/api/apiClient/apiClient";
+
+const getUserInfo = async (token) => {
+    console.log(token);
+    try {
+        const response = await apiClient.get("/api/me", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        });
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default getUserInfo;
