066a1dd2e4cb8a54e928bc23d25fbece87033837
Klimpavlov <Klimpavlov2002@gmail.com>
2025-02-22 23:30:25 +0300

    fix: cors error

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,13 @@ const formRoutes = require('./routes/formRoutes');
 const answerRoutes = require('./routes/answerRoutes');
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
 app.use(bodyParser.json());
 
 // app.use(express.json());
