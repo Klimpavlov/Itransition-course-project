b60dc9445ce198ba3b3f564a5cfa55329eedd434
Klimpavlov <Klimpavlov2002@gmail.com>
2025-03-06 16:02:01 +0300

    fix: jira integration

--- frontend/src/app/api/jira/ticket/getTickets/route.js
+++ frontend/src/app/api/jira/ticket/getTickets/route.js
@@ -6,6 +6,11 @@ const JIRA_EMAIL = "klimpavlov2002@gmail.com";
 const JIRA_API_TOKEN = "ATATT3xFfGF0R_IpcqY5X8YRDmXizc2DkwfIgR14N_nA7i6F0GwiHziM4Su9ZNGJNqLi2wHS2nRFMpC_-F5Itay92ed86CnxOBA17j_gCS13OTHrxLQl0ZEAUxPVHtM9MCoLSI2KuJhp32SwzEkP1L2rfZYTkhTxisGCm1DDkH2g_xcvIwNf2AE=62D76C70";
 const JIRA_PROJECT_KEY = "SCRUM";
 
+// const JIRA_DOMAIN = process.env.JIRA_DOMAIN;
+// const JIRA_EMAIL = process.env.JIRA_EMAIL;
+// const JIRA_API_TOKEN = process.env.JIRA_API_TOKEN;
+// const JIRA_PROJECT_KEY = process.env.JIRA_PROJECT_KEY;
+
 export async function GET() {
     try {
         const response = await axios.get(
